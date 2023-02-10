3cc1bbdf2ea48d5003ab4e7d904156149efd78ca
Abhilash6766 <123532620+Abhilash6766@users.noreply.github.com>
2023-02-10 21:11:53 +0530

    Add files via upload

---
+++ primeseries.py
@@ -0,0 +1,9 @@
+num = int(input("enter a num: "))
+count = 0
+for i in range(1,num+1) :
+    count=0
+    for j in range(1,i+1) :
+        if i%j == 0 :
+            count += 1
+    if count == 2 :
+        print(i,",")
